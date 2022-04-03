efefabc320d13030970aa0451ad7942b934c20fc
함궈 <heoja0123@naver.com>
2022-04-03 20:42:19 +0900

    22-04-03
    request 스코프 문제 해결 - Provider 사용

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -3,6 +3,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,7 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
 
 
@@ -22,6 +23,7 @@ public class LogDemoController {
     @ResponseBody
     public String logDemo(HttpServletRequest request){ //??????????
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
